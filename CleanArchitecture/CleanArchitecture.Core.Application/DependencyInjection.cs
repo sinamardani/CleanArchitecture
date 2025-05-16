ce3016c29f884db2c25424ab9efabb4eb85e0691
Sina <s.mardani2343@gmail.com>
2025-05-16 14:52:28 +0330

    DependencyInjection added to project

---
+++ CleanArchitecture/CleanArchitecture.Core.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Core.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection service)
+    {
+
+        return service;
+    }
+}
