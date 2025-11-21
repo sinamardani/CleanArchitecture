4b3ad9e46e41d9b42545c0fa1c2d342dc5e1ffa2
Sina <s.mardani2343@gmail.com>
2025-11-21 19:49:02 +0330

    Create a Constants class

---
+++ CleanArchitecture/Domain/Commons/Constants/Policies.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Commons.Constants;
+
+public abstract class Policies
+{
+    /// <summary>
+    /// Default policy
+    /// </summary>
+    public const string CanPurge = nameof(CanPurge);
+    /// <summary>
+    /// Combining two roles : Admin and User
+    /// </summary>
+    public const string AdminOrUser = nameof(AdminOrUser);
+}
---
+++ CleanArchitecture/Domain/Commons/Constants/Roles.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Commons.Constants;
+
+public abstract class Roles
+{
+    /// <summary>
+    /// Administrator ROle 
+    /// </summary>
+    public const string Administrator = nameof(Administrator);
+    /// <summary>
+    /// User Role
+    /// </summary>
+    public const string User = nameof(User);
+}
