a56b2f22320bc2e7eb7415a3dfd374e0300f589a
Sina <s.mardani2343@gmail.com>
2025-11-21 20:03:38 +0330

    Implementation interfaces

---
+++ CleanArchitecture/Domain/Commons/BaseValueObject.cs
@@ -0,0 +1,44 @@
+﻿namespace Domain.Commons;
+
+public abstract class BaseValueObject
+{
+    protected static bool EqualOperator(BaseValueObject? left, BaseValueObject? right)
+    {
+        if (left is null ^ right is null)
+        {
+            return false;
+        }
+
+        return left?.Equals(right!) != false;
+    }
+
+    protected static bool NotEqualOperator(BaseValueObject left, BaseValueObject right)
+    {
+        return !(EqualOperator(left, right));
+    }
+
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (BaseValueObject)obj;
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+
+        foreach (var component in GetEqualityComponents())
+        {
+            hash.Add(component);
+        }
+
+        return hash.ToHashCode();
+    }
+}
