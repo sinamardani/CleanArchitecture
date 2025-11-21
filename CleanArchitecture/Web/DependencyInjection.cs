bb1db08b795201455a2175fe4a1ff84a0390a116
Sina <s.mardani2343@gmail.com>
2025-11-21 19:20:27 +0330

    Swagger config added to project

--- CleanArchitecture/Web/DependencyInjection.cs
+++ CleanArchitecture/Web/DependencyInjection.cs
@@ -1,6 +1,66 @@
-﻿namespace Web;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using NSwag;
+using NSwag.Generation.Processors.Contexts;
+using NSwag.Generation.Processors;
+
+namespace Web;
 
 public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddWebServices(this IServiceCollection services) =>
+        services
+            .AddSwagger();
+
+    private static IServiceCollection AddSwagger(this IServiceCollection service)
+    {
+        service.AddEndpointsApiExplorer();
+
+        service.AddOpenApiDocument(option =>
+        {
+            option.Version = "V1.0.0";
+            option.Title = "CleanArchitecture API";
+            option.OperationProcessors.Add(new FormDataOperationProcessor());
+        });
+
+        return service;
+    }
+
+    private class FormDataOperationProcessor : IOperationProcessor
+    {
+        public bool Process(OperationProcessorContext context)
+        {
+            var hasConsumesMultipartAttr = context.MethodInfo?
+                .GetCustomAttributes(typeof(ConsumesAttribute), inherit: true)
+                .Cast<ConsumesAttribute>()
+                .Any(attr => attr.ContentTypes.Contains("multipart/form-data")) ?? false;
+
+            if (!hasConsumesMultipartAttr)
+                return true;
+
+            var modelParam = context.MethodInfo?.GetParameters()
+                .FirstOrDefault(p => p.GetCustomAttribute<FromFormAttribute>() != null);
+
+            if (modelParam is null)
+                return true;
+
+            var modelType = modelParam.ParameterType;
+
+            var schema = context.SchemaGenerator.Generate(modelType, context.SchemaResolver);
+
+            context.OperationDescription.Operation.RequestBody = new OpenApiRequestBody
+            {
+                Content =
+                {
+                    ["multipart/form-data"] = new OpenApiMediaType
+                    {
+                        Schema = schema
+                    }
+                },
+                IsRequired = true
+            };
+
+            return true;
+        }
+    }
 }
--- CleanArchitecture/Web/Program.cs
+++ CleanArchitecture/Web/Program.cs
@@ -1,6 +1,23 @@
+using Web;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddWebServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseOpenApi();
+    app.UseSwaggerUi(setting => setting.Path = "/swagger");
+    app.UseReDoc(setting => setting.Path = "/document");
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.Run();
+
+public partial class Program;
